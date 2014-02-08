f3346011626861830b52f0e87595ffaa7ef60165
Roman Rädle <roman.raedle@outlook.com>
2014-02-07 19:04:28 -0500

    Add to latest commit
    Former-commit-id: 40b1ec955aa70ad83a0c3f6df5cfd02e77d6e37b

---
+++ Tracking/Data/LocationData.cs
@@ -0,0 +1,134 @@
+﻿namespace Tools.FlockingDevice.Tracking.Data
+{
+    public class LocationData : BaseData
+    {
+        #region properties
+
+        #region X
+
+        /// <summary>
+        /// The <see cref="X" /> property's name.
+        /// </summary>
+        public const string XPropertyName = "X";
+
+        private double _x = 0.0;
+
+        /// <summary>
+        /// Sets and gets the X property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+
+            set
+            {
+                if (_x == value)
+                {
+                    return;
+                }
+
+                RaisePropertyChanging(XPropertyName);
+                _x = value;
+                RaisePropertyChanged(XPropertyName);
+            }
+        }
+
+        #endregion
+
+        #region Y
+
+        /// <summary>
+        /// The <see cref="Y" /> property's name.
+        /// </summary>
+        public const string YPropertyName = "Y";
+
+        private double _y = 0.0;
+
+        /// <summary>
+        /// Sets and gets the Y property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+
+            set
+            {
+                if (_y == value)
+                {
+                    return;
+                }
+
+                RaisePropertyChanging(YPropertyName);
+                _y = value;
+                RaisePropertyChanged(YPropertyName);
+            }
+        }
+
+        #endregion
+
+        #region Angle
+
+        /// <summary>
+        /// The <see cref="Angle" /> property's name.
+        /// </summary>
+        public const string AnglePropertyName = "Angle";
+
+        private double _angle = 0.0;
+
+        /// <summary>
+        /// Sets and gets the Angle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public double Angle
+        {
+            get
+            {
+                return _angle;
+            }
+
+            set
+            {
+                if (_angle == value)
+                {
+                    return;
+                }
+
+                RaisePropertyChanging(AnglePropertyName);
+                _angle = value;
+                RaisePropertyChanged(AnglePropertyName);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        public LocationData(string key) : base(key)
+        {
+
+        }
+
+        public override IData Copy()
+        {
+            return new LocationData(Key)
+            {
+                X = X,
+                Y = Y,
+                Angle = Angle
+            };
+        }
+
+        public override void Dispose()
+        {
+            
+        }
+    }
+}
