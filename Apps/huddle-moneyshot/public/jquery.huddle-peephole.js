5263ea5e9372c3dc663f7c8b4186032c406f2419
Roman Rädle <roman.raedle@outlook.com>
2014-04-11 17:41:13 +0200

    Working drag/drop demo -> money money money(shot), we're almost there
    Former-commit-id: 3f65eb2393103af776cec604217046480e4460a7

--- Apps/huddle-moneyshot/public/jquery.huddle-peephole.js
+++ Apps/huddle-moneyshot/public/jquery.huddle-peephole.js
@@ -3,6 +3,15 @@
     // an easy gesture function
     // will make a jQuery object move/scale/rotate on touch(es)
     $.fn.interactive = function (options) {
+
+        var Unlock = -1;
+        var Actions = {
+            None: 'None',
+            PickDragDrop: 'PickDragDrop',
+            Drag: 'Drag',
+            DragScaleRotate: 'DragScaleRotate',
+        };
+
         if (!options || typeof (options) != "object") {
             options = {};
         }
@@ -20,7 +29,9 @@
                 touchtarget: null,
 
                 visualProperties: {
-                    lockedBy: -1,
+                    lockedBy: Unlock,
+                    lockedForAction: Actions.None,
+                    lockedData: '',
                     x: 0,
                     y: 0,
                     width: 100,
@@ -29,12 +40,14 @@
                     scale: 1.0
                 },
 
+                /* Currently a global peepholeMetadata is used
                 peepholeMetadata: {
                     scaleX: 1.0,
                     scaleY: 1.0,
                     minVisualScale: 0.2,
                     maxVisualScale: 2.5
                 },
+                */
 
                 modelUpdated: null,
             },
@@ -50,12 +63,19 @@
         var vp = options.visualProperties;
         $visual.data('visualProperties', vp);
 
+        var updateModel = function() {
+            // call model updated function with that as context
+            var vp = $visual.data('visualProperties');
+            modelUpdated.call(that, vp);
+        };
+
         var getParameterByName = function(name) {
             name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
             var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
                 results = regex.exec(location.search);
             return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-        }
+        };
+
         // get device id -> used to lock objects
         var deviceId = getParameterByName("id");
 
@@ -91,9 +111,7 @@
             $visual.css('-webkit-transform', transform);
 
             if (modelUpdated != null && typeof modelUpdated == 'function') {
-                // call model updated function with that as context
-                var vp = $visual.data('visualProperties');
-                modelUpdated.call(that, vp);
+                updateModel();
             }
         };
 
@@ -104,6 +122,7 @@
           };
 
         var isScaleRotate = false;
+        var isPickDrag = false;
 
         var dragHandler = function(e) {
             if (isScaleRotate) return;
@@ -111,7 +130,7 @@
             var vp = $visual.data('visualProperties');
 
             if (vp.lockedBy &&
-                (vp.lockedBy != -1 && vp.lockedBy != deviceId)) return;
+                (vp.lockedBy != Unlock && vp.lockedBy != deviceId)) return;
 
             for (var i = 0; i < e.touches.length; i++) {
                 if (e.touches[i].handled) {
@@ -156,7 +175,7 @@
             }
 
             if (e.type == '_gesture2_touch_end') {
-                vp.lockedBy = -1;
+                vp.lockedBy = Unlock;
 
                 doVisualTransform(null);
             }
@@ -179,7 +198,7 @@
             var vp = $visual.data('visualProperties');
 
             if (vp.lockedBy &&
-                (vp.lockedBy != -1 && vp.lockedBy != deviceId)) return;
+                (vp.lockedBy != Unlock && vp.lockedBy != deviceId)) return;
 
             if (e.type == '_gesture2_gesture_start') {
                 //console.log('_gesture2_gesture_start');
@@ -252,7 +271,7 @@
                 // activates drag handler
                 isScaleRotate = false;
 
-                vp.lockedBy = -1;
+                vp.lockedBy = Unlock;
 
                 doVisualTransform(null);
             }
@@ -270,6 +289,152 @@
         touchtarget.on("_gesture2_gesture_start", rotateScaleHandler);
         touchtarget.on("_gesture2_gesture_move", rotateScaleHandler);
         touchtarget.on("_gesture2_gesture_end", rotateScaleHandler);
+
+        var startPosition = {
+            x: 0,
+            y: 0,
+            scale: 1.0
+        };
+
+        var currentHandId = -1;
+
+        var pickDragDropHandler = function(e) {
+            var vp = $visual.data('visualProperties');
+
+            //console.log(e.type + ", id: " + e.id);
+
+            if (e.type == 'presence_start' ||
+                e.type == 'presence_end') {
+                currentHandId = e.id;
+            }
+
+            // perform object move only on action source device
+            if (vp.lockedBy &&
+                (vp.lockedBy != Unlock && vp.lockedBy != deviceId)) return;
+
+            // move object only by hand that initially started the pick drag drop
+            // the hand id is stored in the locked data on 'tap' event
+            if (vp.lockedData != e.id) return;
+
+            if (e.type == 'presence_end') {
+                if (vp.lockedForAction == Actions.PickDragDrop) {
+
+                    console.log('presence end on device: ' + deviceId);
+                    
+                    touchtarget.off("presence_move", pickDragDropHandler);
+                    onPresenceMove = null;
+
+                    vp.lockedForAction = Actions.None;
+                    vp.lockedBy = Unlock;
+
+                    vp.x = startPosition.x;
+                    vp.y = startPosition.y;
+                    //vp.scale = startPosition.scale;
+
+                    //console.log('reset pick drag drop to: ' + startPosition.x + ',' + startPosition.y + ',' + startPosition.scale);
+                }
+            }
+            else if (e.type == 'presence_move') {
+                // ignore event when action is not a pick drag drop action
+                if (vp.lockedForAction != Actions.PickDragDrop) return;
+
+                var canvasWidth = peepholeMetadata.canvasWidth;
+                var canvasHeight = peepholeMetadata.canvasHeight;
+
+                var targetWidth = touchtarget.width() / 2;
+                var targetHeight = touchtarget.height() / 2;
+                var offsetX = 0;
+                var offsetY = canvasHeight * 0.2;
+
+                var x = (e.x * canvasWidth) - targetWidth - offsetX;
+                var y = (e.y * canvasHeight) - targetHeight - offsetY;
+                 
+                console.log('touchtarget width/height: ' + targetWidth + ',' + targetHeight);
+
+                var depthScale = (1 + (e.z / 200)) * 1.5;
+
+                vp.x = x;
+                vp.y = y;
+                //vp.scale = startScale * depthScale;
+
+                //console.log('x: ' + vp.x + ', y: ' + vp.y + ', z: ' + vp.scale + ', depthScale: ' + depthScale);
+            }
+
+            doVisualTransform(null); 
+        };
+
+        var startScale = 1.0;
+
+        /*
+        hammer.on('tap', function() {
+            var vp = $visual.data('visualProperties');
+
+            if (vp.lockedForAction == Actions.PickDragDrop) {
+                console.log('tap happened with lock for action: ' + vp.lockedForAction);
+            }
+            //isPickDrag = false;
+        });
+        */
+
+        touchtarget.on('tap', function(e) {
+
+            //console.log("i was tapped");
+
+            e.stopPropagation();
+            e.preventDefault();
+
+            //console.log('Target Object: ' + e.target);
+
+            var vp = $visual.data('visualProperties');
+            
+            if (vp.lockedForAction == Actions.PickDragDrop) {
+                //console.log('remove presence move');
+
+                touchtarget.off("presence_move", pickDragDropHandler);
+                onPresenceMove = null;
+
+                // reset scale because depth value of tracking is not yet reliable
+                //vp.scale = startPosition.scale;
+
+                vp.lockedBy = Unlock;
+                vp.lockedForAction = Actions.None;
+                vp.lockedData = '';
+            }
+            else {
+                vp.lockedBy = deviceId;
+                vp.lockedData = currentHandId;
+
+                startScale = vp.scale;
+                vp.lockedForAction = Actions.PickDragDrop;
+
+                startPosition.x = vp.x;
+                startPosition.y = vp.y;
+                startPosition.scale = vp.scale;
+
+                //console.log('start pick drag drop at: ' + startPosition.x + ',' + startPosition.y + ',' + startPosition.scale);
+
+                touchtarget.on("presence_move", pickDragDropHandler);
+
+                //console.log('add presence move');
+            }
+
+            console.log('tapped: ' + vp.lockedForAction);
+
+            //$visual.data('visualProperties', vp);
+            
+            updateModel();
+        });
+
+        touchtarget.on("presence_start", pickDragDropHandler);
+        touchtarget.on("presence_end", pickDragDropHandler);
+
+        //var presenceHandler = function(e) {
+        //    console.log('Event Type: ' + e.type + ', id: ' + e.id);
+        //}
+        //
+        //touchtarget.on('presence_start', presenceHandler);
+        //touchtarget.on('presence_move', presenceHandler);
+        //touchtarget.on('presence_end', presenceHandler);
 
         return this;
     };
