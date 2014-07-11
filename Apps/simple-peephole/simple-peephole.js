d596ab672e9a0b998b8bb1ef378bd8a294eff2b7
Roman Rädle <roman.raedle@outlook.com>
2014-07-11 22:01:20 +0200

    Simple peephole app shows a modal dialog on startup to enter Huddle Engine/HuddleOrbiter host, port, and client name. This avoids hard coded host and port.
    Former-commit-id: 7a6edbb25e2ce385f1a53f0195d4fd5e9ee09b83

--- Apps/simple-peephole/simple-peephole.js
+++ Apps/simple-peephole/simple-peephole.js
@@ -19,44 +19,60 @@ if (Meteor.isClient) {
         });
       };
 
-      var huddle = Huddle.client("simple-peephole")
-        .on("proximity", function(data) {
-          var location = data.Location;
-          var x = location[0];
-          var y = location[1];
-          var angle = data.Orientation;
+      var hutHutHut = function(host, port, name) {
+        var huddle = Huddle.client(name)
+          .on("proximity", function(data) {
 
-          data.Presences.forEach(function(presence) {
-              var id2 = presence.Identity;
-              var location2 = presence.Location.split(",");
-              var x2 = location2[0];
-              var y2 = location2[1];
+            var location = data.Location;
+            var x = location[0];
+            var y = location[1];
+            var angle = data.Orientation;
 
-              var $presence = $('#presence-' + id2);
+            data.Presences.forEach(function(presence) {
+                var id2 = presence.Identity;
+                var location2 = presence.Location.split(",");
+                var x2 = location2[0];
+                var y2 = location2[1];
 
-              if (!$presence.length) {
-                  $('<div id="presence-' + id2 + '" class="huddle-presence">' + id2 + '</div>').appendTo($('#presences-container'));
-              } else {
-                  $presence.html(JSON.stringify(presence));
+                var $presence = $('#presence-' + id2);
 
-                  $presence.css('height', ($(window).height()) + "px");
-                  $presence.css('top', "-" + ($(window).height() / 2) + "px");
-                  $presence.rotate(presence.Orientation);
-              }
+                if (!$presence.length) {
+                    $('<div id="presence-' + id2 + '" class="huddle-presence">' + id2 + '</div>').appendTo($('#presences-container'));
+                } else {
+                    $presence.html(JSON.stringify(presence));
+
+                    $presence.css('height', ($(window).height()) + "px");
+                    $presence.css('top', "-" + ($(window).height() / 2) + "px");
+                    $presence.rotate(presence.Orientation);
+                }
+            });
+
+            var width = $('#peephole-canvas').width(); // 3840
+            var height = $('#peephole-canvas').height(); // 2160
+
+            var offsetLeft = width * x - (windowWidth / 2);
+            var offsetTop = height * y - (windowHeight / 2);
+
+            $('#peephole-canvas').css('left', -offsetLeft + "px");
+            $('#peephole-canvas').css('top', -offsetTop + "px");
           });
+          huddle.connect(host, port);
+      };
 
-          var width = $('#peephole-canvas').width(); // 3840
-          var height = $('#peephole-canvas').height(); // 2160
+      $('#connection-dialog').modal({
+        backdrop: false,
+        keyboard: true,
+        show: true
+      });
+      $('#connection-dialog').on('hidden.bs.modal', function (e) {
+        var host = $('#client-host').val();
+        var port = parseInt($('#client-port').val());
+        var name = $('#client-name').val();
 
-          var offsetLeft = width * x - (windowWidth / 2);
-          var offsetTop = height * y - (windowHeight / 2);
-
-          $('#peephole-canvas').css('left', -offsetLeft + "px");
-          $('#peephole-canvas').css('top', -offsetTop + "px");
-        });
-        huddle.connect("localhost", 4711);
+        hutHutHut(host, port, name);
+      });
     });
-  };
+  }
 }
 
 if (Meteor.isServer) {
