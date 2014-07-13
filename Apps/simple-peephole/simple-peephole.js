9a857568bfffc801f9cd009ba09cac881c17babe
Roman Rädle <roman.raedle@outlook.com>
2014-07-13 02:47:17 +0200

    Use placeholders for connection dialog in simple-peephole app. Refactored methods in simple-peephole app.
    Former-commit-id: 8aac2c699acfb39d6cf71e04140a7b4abbee2ec6

--- Apps/simple-peephole/simple-peephole.js
+++ Apps/simple-peephole/simple-peephole.js
@@ -1,5 +1,7 @@
 if (Meteor.isClient) {
 
+  var Peephole = {};
+
   /**
    * Adds an additional function rotate as jQuery function.
    */
@@ -17,15 +19,12 @@ if (Meteor.isClient) {
    */
   Template.main.rendered = function() {
 
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-
     /**
      * Move the canvas according to the current proximity.
      *
      * @param {Object} proximity Proximity data that moves the canvas.
      */
-    var moveCanvas = function(proximity) {
+    Peephole.moveCanvas = function(proximity) {
       var location = proximity.Location;
       var x = location[0];
       var y = location[1];
@@ -34,8 +33,8 @@ if (Meteor.isClient) {
       var width = $('#peephole-canvas').width(); // 3840
       var height = $('#peephole-canvas').height(); // 2160
 
-      var offsetLeft = width * x - (windowWidth / 2);
-      var offsetTop = height * y - (windowHeight / 2);
+      var offsetLeft = width * x - ($(window).width() / 2);
+      var offsetTop = height * y - ($(window).height() / 2);
 
       $('#peephole-canvas').css('left', -offsetLeft + "px");
       $('#peephole-canvas').css('top', -offsetTop + "px");
@@ -47,7 +46,7 @@ if (Meteor.isClient) {
      *
      * @param {Object} presences Other presences.
      */
-    var renderPresences = function(presences) {
+    Peephole.renderPresences = function(presences) {
       presences.forEach(function(presence) {
           var id2 = presence.Identity;
           var location2 = presence.Location;
@@ -88,15 +87,15 @@ if (Meteor.isClient) {
      * @param {int} port Huddle Engine port.
      * @param {string} [name] Huddle client's name.
      */
-    var hutHutHut = function(host, port, name) {
+    Peephole.hutHutHut = function(host, port, name) {
       var huddle = Huddle.client(name)
         .on("proximity", function(data) {
 
           // move canvas
-          moveCanvas(data);
+          Peephole.moveCanvas(data);
 
           // render presence indicators
-          renderPresences(data.Presences);
+          Peephole.renderPresences(data.Presences);
         });
         huddle.connect(host, port);
     };
@@ -106,18 +105,36 @@ if (Meteor.isClient) {
    * Render the connection dialog.
    */
   Template.connectionDialog.rendered = function() {
-    $('#connection-dialog').modal({
-      backdrop: false,
-      keyboard: true,
-      show: true
-    });
-    $('#connection-dialog').on('hidden.bs.modal', function (e) {
-      var host = $('#client-host').val();
-      var port = parseInt($('#client-port').val());
-      var name = $('#client-name').val();
 
-      hutHutHut(host, port, name);
-    });
+    var host = Session.get("clientHost");
+    var port = Session.get("clientPort");
+    var name = Session.get("clientName");
+
+    console.log(host);
+
+    if (host && port && name) {
+      Peephole.hutHutHut(host, port, name);
+    }
+    else {
+      $('#connection-dialog').modal({
+        backdrop: false,
+        keyboard: false,
+        show: true
+      });
+      $('#connection-dialog').on('hidden.bs.modal', function (e) {
+        var host = $('#client-host').val();
+        var port = parseInt($('#client-port').val());
+        var name = $('#client-name').val();
+
+        Session.set("clientHost", host);
+        Session.set("clientPort", port);
+        Session.set("clientName", name);
+
+        console.log("ASDF: " + Session.get("clientHost"));
+
+        Peephole.hutHutHut(host, port, name);
+      });
+    }
   };
 }
 
