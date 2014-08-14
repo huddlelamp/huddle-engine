afb11a0badce5a64531d95e8ddb8e28a52d69c6a
Mario Schreiner <mario.schreiner@uni-konstanz.de>
2014-08-14 10:34:33 +0200

    Added the possibility for enter phrases into the search field by using either single or double quotes. Phrases can also be negated by adding a minus before the opening quotation mark.
    Former-commit-id: fd48b1fe7670b58a8b91c7e3ebefe657f0fdbb36

--- Apps/spatial-sensemaking/client/search/search.js
+++ Apps/spatial-sensemaking/client/search/search.js
@@ -2,57 +2,91 @@ if (Meteor.isClient) {
 
   var search = function(query) {
 
+    var SEARCH_FIELDS = ["file^10", "_name"];
+
     var terms = query.split(" ");
 
-    var must = "";
-    var must_not = "";
+    var must = [];
+    var must_not = [];
 
     var term;
-    for (var i = terms.length - 1; i >= 0; i--) {
-      term = terms[i];
+    var quoteOpen;
+    var not = false;
 
-      if (term.charAt(0) === '-') {
-        must_not += " " + term.substring(1, term.length);
-      }
-      else {
-        must += " " + term;
+    //Some rather complex code for building queries, but here we go...
+    //We walk over every (space-separated) term. Every term is converted into an
+    //entry usable by elasticsearch. The entries are added to must/must_not, when:
+    //    * A normal term is simply added to the must array
+    //    * A term prefixed by '-' is added to the must_not arrya
+    //    * If a term begins with a quote, it starts a phrase. The term is not added yet
+    //    * The next term ending with a quote ends the phrase. The entire phrase is added 
+    //      to either must or must_not with the type "phrase"
+    for (var i = 0; i < terms.length; i++) {
+      if (quoteOpen === undefined) {
+        term = terms[i];
+        not = false;
+
+        //Check for negation prefix
+        if (term.charAt(0) === '-') {
+          term = term.substring(1, term.length);
+          not = true;
+        }
+
+        //Check for opening quotes
+        if (term.charAt(0) === "'" || term.charAt(0) === '"') {
+          quotesOpen = term.charAt(0);
+          term = term.substring(1, term.length);
+        } else {
+          var entry = {
+            multi_match: {
+              query: term,
+              fields: SEARCH_FIELDS
+            }
+          };
+
+          if (not)  must_not.push(entry);
+          else      must.push(entry);
+        }
+      } else {
+        //A phrase is still open, add the term to the existing phrase
+        term += " " + terms[i];
+
+        //Check if the phrase ends here
+        if (term.charAt(term.length-1) === quotesOpen) {
+          term = term.substring(0, term.length-1);
+          quotesOpen = undefined;
+
+          var phraseEntry = {
+            multi_match: {
+              query: term,
+              fields: SEARCH_FIELDS,
+              type: "phrase"
+            }
+          };
+
+          if (not)  must_not.push(phraseEntry);
+          else      must.push(phraseEntry);
+        }
       }
     }
 
-    console.log(must);
-    console.log(must_not);
+    //elasticsearch can't handle empty must/must_not blocks
+    var bool = {};
+    if (must.length > 0) bool.must = must;
+    if (must_not.length > 0) bool.must_not = must_not;
 
     var q = {
       fields: ["file"],
       from: 0,
       size: 10,
-      query: {
-        bool: {
-          must: [
-            {
-              multi_match: {
-                query: must,
-                fields: ["file^10", "_name"]
-              }
-            }
-          ],
-          must_not: [
-            {
-              multi_match: {
-                query: must_not,
-                fields: ["file^10", "_name"]
-              }
-            }
-          ],
-          // should: { match: { file: "Police" } },
-        }
-      },
+      query: { bool: bool },
       highlight: {
         fields: {
           file: {}
         }
       }
     };
+    console.log(q);
 
     ElasticSearch.query(q, function(err, result) {
       if (err) {
