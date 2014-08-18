4b327507d6dd30a3e06d4ce48a0e8453f869bc6f
Mario Schreiner <mario.schreiner@uni-konstanz.de>
2014-08-19 01:59:33 +0200

    Added Huddle and Huddle Orbiter. Added device presence indicators at the edge of the browser window. Pretty cool.
    Former-commit-id: f7de6c96253911379c0ce7367eccd4a4420d236d

--- Apps/spatial-sensemaking/app.js
+++ Apps/spatial-sensemaking/app.js
@@ -1,3 +1,62 @@
 if (Meteor.isClient) {
 
+  $(function() {
+    var transformDeviceData = function(data) {
+      var newData = {
+        id: data.Identity,
+        topLeft: {
+          x: data.Location[0] || 0,
+          y: data.Location[1] || 0,
+        },
+        ratio: {
+          x: data.RgbImageToDisplayRatio.X || 1,
+          y: data.RgbImageToDisplayRatio.Y || 1
+        },
+        angle: data.Orientation || 0,
+      };
+
+      newData.width = 1.0/newData.ratio.x;
+      newData.height = 1.0/newData.ratio.y;
+
+      newData.topRight = {
+        x: newData.topLeft.x + newData.width, 
+        y: newData.topLeft.y
+      };
+
+      newData.bottomRight = {
+        x: newData.topLeft.x + newData.width, 
+        y: newData.topLeft.y + newData.height
+      };
+
+      newData.bottomLeft = {
+        x: newData.topLeft.x, 
+        y: newData.topLeft.y + newData.height
+      };
+
+      newData.center = { 
+        x: newData.topLeft.x + newData.width/2.0, 
+        y: newData.topLeft.y + newData.height/2.0
+      };
+
+      return newData;
+    };
+
+    var huddle = Huddle.client("MyHuddleName")
+      .on("proximity", function(data) {
+        var location = data.Location;
+        var x = location[0];
+        var y = location[1];
+        var angle = data.Orientation;
+
+        Session.set('thisDevice', transformDeviceData(data));
+
+        var otherDevices = [];
+        data.Presences.forEach(function(presence) {
+          otherDevices.push(transformDeviceData(presence));
+        });
+        Session.set('otherDevices', otherDevices);
+      });
+    huddle.connect("huddle-orbiter.proxemicinteractions.org", 47060);
+  });
+
 }
--- Apps/spatial-sensemaking/client/search/search.js
+++ Apps/spatial-sensemaking/client/search/search.js
@@ -96,6 +96,10 @@ if (Meteor.isClient) {
     return Session.get("querySuggestions") || [];
   };
 
+  Template.searchIndex.otherDevices = function() {
+    return Session.get("otherDevices") || [];
+  };
+
   Template.searchIndex.hasComment = function() {
     var meta = DocumentMeta.findOne({_id : this._id});
     return (meta && ((meta.comment && meta.comment.length > 0) || (meta.textHighlights && meta.textHighlights.length > 0)));
@@ -107,6 +111,113 @@ if (Meteor.isClient) {
   };
 
   Template.searchIndex.helpers({
+    'devicePositionCSS': function() {
+      var indicatorSize = 60;
+
+      var intersect = function(p1, p2, p3, p4) {
+        var m1 = (p1.y-p2.y)/(p1.x-p2.x);
+        var m2 = (p3.y-p4.y)/(p3.x-p4.x);
+
+        if (isNaN(m1) || !isFinite(m1)) m1 = 0;
+        if (isNaN(m2) || !isFinite(m2)) m2 = 0;
+
+        // if (m1 === 0 && m2 === 0) {
+        //   //First line is parallel to x, second parallel to y
+        //   if ((p1.y-p2.y) === 0 && (p3.x-p4.x) === 0) {
+        //     return { x: p3.x, y: p1.y };
+        //   }
+        //   //Other way around
+        //   if ((p1.x-p2.x) === 0 && (p3.y-p4.y) === 0) {
+        //     return { x: p1.x, y: p3.y };
+        //   }
+        // }
+
+        if (m1 === m2 && m1 !== 0) return undefined;
+
+        var c1 = p1.y - m1 * p1.x;
+        var c2 = p3.y - m2 * p3.x;
+
+        var ix = (c2-c1)/(m1-m2);
+        if ((p1.x-p2.x) === 0) ix = p1.x;
+        if ((p3.x-p4.x) === 0) ix = p3.x;
+
+        var iy = m1 * ix + c1;
+        if ((p1.y-p2.y) === 0) iy = p1.y;
+        if ((p3.y-p4.y) === 0) iy = p3.y;
+
+        return { x: ix, y: iy };
+      };
+
+      function insideDevice(p, device) {
+        return (
+          p.y >= device.topLeft.y && 
+          p.y <= device.bottomLeft.y &&
+          p.x >= device.topLeft.x &&
+          p.x <= device.topRight.x
+        );
+      }
+
+      function pointDist(p1, p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+      }
+
+      var thisDevice = Session.get('thisDevice');
+      var otherDevice = this;
+
+      // console.log(thisDevice);
+
+      var intersectLeft   = intersect(thisDevice.center, otherDevice.center, thisDevice.topLeft,    thisDevice.bottomLeft);
+      var intersectTop    = intersect(thisDevice.center, otherDevice.center, thisDevice.topLeft,    thisDevice.topRight);
+      var intersectRight  = intersect(thisDevice.center, otherDevice.center, thisDevice.topRight,   thisDevice.bottomRight);
+      var intersectBottom = intersect(thisDevice.center, otherDevice.center, thisDevice.bottomLeft, thisDevice.bottomRight);
+      
+      // console.log("----");
+      // console.log(thisDevice);
+      // console.log(intersectLeft);
+      // console.log(intersectTop);
+      // console.log(intersectRight);
+      // console.log(intersectBottom);
+
+      var leftDist   = insideDevice(intersectLeft,   thisDevice) ? pointDist(intersectLeft,   otherDevice.center) : 4000;
+      var topDist    = insideDevice(intersectTop,    thisDevice) ? pointDist(intersectTop,    otherDevice.center) : 4000;
+      var rightDist  = insideDevice(intersectRight,  thisDevice) ? pointDist(intersectRight,  otherDevice.center) : 4000;
+      var bottomDist = insideDevice(intersectBottom, thisDevice) ? pointDist(intersectBottom, otherDevice.center) : 4000;
+
+      // console.log(leftDist);
+      // console.log(topDist);
+      // console.log(rightDist);
+      // console.log(bottomDist);
+
+      var finalX;
+      var finalY;
+      if (leftDist <= topDist && leftDist <= rightDist && leftDist <= bottomDist) {
+        finalY = $(window).height() * ((intersectLeft.y - thisDevice.topLeft.y) / thisDevice.height);
+        finalX = -(indicatorSize/2.0);
+      } else if (topDist <= leftDist && topDist <= rightDist && topDist <= bottomDist) {
+        finalY = -(indicatorSize/2.0);
+        finalX = $(window).width() * ((intersectTop.x - thisDevice.topLeft.x) / thisDevice.width);
+      } else if (rightDist <= leftDist && rightDist <= topDist && rightDist <= bottomDist) {
+        finalY = $(window).height() * ((intersectRight.y - thisDevice.topRight.y) / thisDevice.height);
+        finalX = $(window).width()-(indicatorSize/2.0);
+      } else if (bottomDist <= leftDist && bottomDist <= topDist && bottomDist <= rightDist) {
+        finalY = $(window).height()-(indicatorSize/2.0);
+        finalX = $(window).width() * ((intersectBottom.x - thisDevice.bottomLeft.x) / thisDevice.width);
+      }
+
+      finalX = Math.max(-(indicatorSize/2.0), Math.min($(window).width()-(indicatorSize/2.0), finalX));
+      finalY = Math.max(-(indicatorSize/2.0), Math.min($(window).height()-(indicatorSize/2.0), finalY));
+      // var test = (intersectLeft.y - thisDevice.topLeft.y);
+
+      //device height = 1/data.ratio.y;
+      // test/height = percent
+      // device height * percent = wohoo
+      // window.height * (test/(1/data.ratio.y))
+      // test = $(window).height() * (test/(1/thisDevice.ratio.y));
+
+      // console.log(intersectLeft.y+" - "+thisDevice.topLeft.y+" || "+test);
+      return 'top: '+finalY+'px; left: '+finalX+'px;';
+    },
+
     'toSeconds': function(ms) {
       var time = new Date(ms);
       return time.getSeconds() + "." + time.getMilliseconds();
@@ -118,13 +229,13 @@ if (Meteor.isClient) {
       var url = s.protocol + "://" + s.host + ":" + s.port + "/" + IndexSettings.getActiveIndex() + "/" + s.attachmentsPath + "/" + id;
       return url;
     },
+
+    stringify: function(obj) {
+      return JSON.stringify(obj);
+    }
   });
 
   Template.searchIndex.events({
-    'keydown #search-query': function(e, tmpl) {
-      
-    },
-
     'click #search-btn': function(e, tmpl) {
       var query = tmpl.$('#search-query').val();
       search(query);
